ed450d5bdc40e252d8b68ffc1f8120ca4ff41b40
victor <victor@debian.com>
2025-08-14 08:56:21 -0400

    Removing additional spaces

--- Pessoa.java
+++ Pessoa.java
@@ -12,26 +12,21 @@ public class Pessoa {
         setEmail(email);
     }
 
-
     public String getNome() {
         return this.nome;
     }
-
 
     public String getCpf() {
         return this.cpf;
     }
 
-
     public String getTelefone() {
         return this.telefone;
     }
 
-
     public String getEmail() {
         return this.email;
     }
-
 
     public void setNome(String nome) {
         if (nome == null || nome.isEmpty()) {
@@ -42,7 +37,6 @@ public class Pessoa {
         }
     }
 
-
     public void setCpf(String cpf) {
         if (cpf == null || cpf.isEmpty()) {
             throw new IllegalArgumentException("O cpf da pessoa a ser cadastrada não deve ser nulo ou vazio.");
@@ -52,7 +46,6 @@ public class Pessoa {
         }
     }
 
-
     public void setTelefone(String telefone) {
         if (telefone == null || telefone.isEmpty()) {
             throw new IllegalArgumentException("A pessoa a ser cadastrada precisa de um numero de telefone. Nao deixe nulo ou vazio.");
@@ -61,7 +54,6 @@ public class Pessoa {
             this.telefone = telefone;
         }
     }
-
 
     public void setEmail(String email) {
         if (email == null || email.isEmpty()) {
